131e3111ac101a20000fce21505f339e04e4991f
Arjya Tust kashyap <80963174+arjya-kaishyap@users.noreply.github.com>
2022-06-02 17:27:56 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,22 @@
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
+
+
+class ATM :
+    def __init__(self, cardnumber, pin ):
+        self.cardnumber = cardnumber
+        self.pin = pin
+
+    def balenceinquiry(self):
+        new_amount = 100-new_amount
+        print("You balence is : $100")
+
+    def cashwidthdrawl(self, amount):
+        print("Hello, " + name)
+        cardnumber = input("Insert your card number: ")
+        pin = input("Enter your pin: ")
+        new_user = ATM(cardnumber, pin)
+        new_amount = 100-amount
+        print("You withdrawed: " + str(amount) + "Tour remaining balance is: " + str(new_amount))
+        name = input("Hello what is your name?")
+
+
